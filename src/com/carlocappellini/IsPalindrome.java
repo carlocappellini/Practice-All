93a9ecbadee0bfad22dfd17142e1364273cd5f31
Bolimbo <carlo.cappellini@yahoo.com>
2018-09-10 22:07:12 -0500

    Palindrome

--- src/com/carlocappellini/IsPalindrome.java
+++ src/com/carlocappellini/IsPalindrome.java
@@ -3,33 +3,31 @@ package com.carlocappellini;
 public class IsPalindrome {
 
     public static void main(String[] args) {
-        isPalindrome(9999);
+        isPalindrome(999999);
     }
 
     private static boolean isPalindrome(int number) {
         int reverse = 0;
 
-
         while (number != 0) {
+            int remainder = number % 10; // 9
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
 
-            int remainder = number % 10;
-            System.out.println(remainder);
-            reverse = remainder * 10 + remainder;
-            System.out.println(reverse);
-            number = number /10;
             System.out.println(number);
 
+            if (number == reverse || reverse == number) {
 
-            if (number == reverse || remainder == number) {
-
-                System.out.println("Number Is Palindrome ");
+                System.out.println("Palindrome");
                 return true;
-
             }
+
+
         }
+
+
         return false;
     }
-
 }
 
 
